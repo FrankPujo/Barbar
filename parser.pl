bba38f1e1f2f2ec3fbdf61a7db0f7f0a148811be
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2021-03-12 18:47:38 +0100

    Rough html conversion

--- parser.pl
+++ parser.pl
@@ -2,7 +2,6 @@ use strict;
 use warnings;
 
 my $p = chr( 156 );
-print( "$p\n" );
 
 my $content;
 my $filename = "test.barb"; # <STDIN>
@@ -15,6 +14,18 @@ open( my $fh, '<', $filename );
  my @lines = split /\n/, $content;
 
 my $lines_num = scalar @lines;
-for ( my $i = 0,; $i < $lines_num; $i++ ) {
-	print( "$lines[$i]\n" );
+
+my $html_content;
+
+for ( my $i = 0; $i < $lines_num; $i++ ) {
+	my $line = $lines[$i];
+	my @tokens = split / /, $line;
+	my $type = substr $tokens[0], 2;
+	print( "<$type>" );
+	my $tokens_num = scalar @ tokens;
+	#print( $tokens_num )
+	for ( my $j = 1; $j < $tokens_num; $j++ ) {
+		print( "$tokens[$j] " );
+	}
+	print( "</$type>\n" );
 }
